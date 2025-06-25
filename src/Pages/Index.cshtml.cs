34802e8df4205c5e2fbe1d0f4a18a37599b768e6
AniruddhJadhavTR <Aniruddh.Jadhav@thomsonreuters.com>
2025-06-25 13:48:16 +0530

    Initial commit: .NET web app with separate build and code signing jobs

---
+++ src/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+public class IndexModel : PageModel
+{
+    public string CurrentTime { get; set; }
+    public void OnGet()
+    {
+        CurrentTime = DateTime.Now.ToString("F");
+    }
+}
